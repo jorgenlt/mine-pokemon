3ec505bb428965f77d6350d795e1333120c0091b
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-05-23 02:33:17 +0200

    main features working

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react'
 import './styles/app.scss'
-import Search from './components/Search'
+// import Search from './components/Search'
 import Banner from './components/Banner'
 
 function App() {
   const [allPokemon, setAllPokemon] = useState([]);
+  const [query, setQuery] = useState('');
+  const [searchResults, setSearchresults] = useState([]);
+  const [myPokemon, setMyPokemon] = useState([]);
 
   useEffect(() => {
-    const limit = 20;
+    const limit = 50;
     const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}`
 
     fetch(url)
@@ -23,8 +26,15 @@ function App() {
     }
   ,[])
 
+  useEffect(() => {
+    const search = allPokemon.filter(pokemon =>
+      pokemon.name.includes(query.toLowerCase())
+    );
+    setSearchresults(search);
+  }, [query]);
+
   const PokemonData = props => {
-    const [pokemonData, setPokemonData] = useState([]);
+    const [pokemonData, setPokemonData] = useState();
 
     useEffect(() => {
       fetch(props.url)
@@ -36,57 +46,114 @@ function App() {
         .catch(error => {
           console.log(error);
         });
-    }, [])
+    }, [props.url])
 
     if (!pokemonData) {
       return <li>Loading...</li>;
     }
 
-    const pokeStats = pokemonData;
-
-    if (pokeStats) {
-      const image = pokemonData.sprites?.other?.["official-artwork"]?.["front_default"];
-      return (
-        <>
-          <img src={image} width={150}></img>
-          <ul>
-            <li>{pokemonData.stats?.[0].base_stat}</li>
-            <li>{pokemonData.abilities?.[0].ability.name}</li>
-            <li></li>
-            <li></li>
-          </ul>
-        </>
-        )
+    const image = pokemonData.sprites?.other?.["official-artwork"]?.["front_default"];
+    const hp = pokemonData.stats?.[0].base_stat;
+    
+    const ability1 = pokemonData.abilities?.[0].ability?.name;
+    let ability2 = false;
+    if (pokemonData.abilities?.[1]) {
+      ability2 = pokemonData.abilities?.[1].ability?.name;
+      // console.log(ability2);
     }
+
+    return (
+      <>
+        <img src={image} width={150}></img>
+        <ul>
+          <li>{hp}</li>
+          <li>{ability1}</li>
+          {ability2 && <li>{ability2}</li>}
+          <li></li>
+          <li></li>
+        </ul>
+      </>
+    )
   }
 
-  const cardElements = () => {
-    return (
-      allPokemon.map(pokemon => {
-        // console.log(pokemon.name);
-        // const name = pokemon
-        // console.log(name);
+  // const addPokemon = pokemon => {
+  //   setMyPokemon([
+  //     ...myPokemon,
+  //     pokemon
+  //   ])
+  //   console.log(myPokemon);
+  // }
 
+  const cardElements = (pokemon) => {
+    
+    return (
+      pokemon.map(pokemon => {
         return (
-        <div className="card" key={pokemon.name}>
-          <h1 className="name">{pokemon.name.charAt(0).toUpperCase()+ pokemon.name.slice(1)}</h1>
-          <PokemonData url={pokemon.url} />
-        </div>
+          <div className="card" key={pokemon.name}>
+            <h1 className="name">{`${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`}</h1>
+            <PokemonData url={pokemon.url} />
+            <button
+              // onClick={() => addPokemon(pokemon)}
+              onClick={() => myPokemon.includes(pokemon) ? console.log('Pokemon already exists.') : setMyPokemon([...myPokemon, pokemon])}
+            >
+              Legg til i liste
+            </button>
+          </div>
         )
       })
     )
   }
-  
-  // allPokemon.length > 0 ? console.log(allPokemon) : console.log('no pokemon');
 
+  // const deletePokemon = pokemon => {
+  //   setMyPokemon(prev => prev.filter(item => item !== pokemon));
+  // };
+
+  const listElements = myListedPokemon => {
+    if (myListedPokemon) {
+      return (
+        myListedPokemon.map(pokemon => {
+          return (
+            <div className="card" key={pokemon.name}>
+              <h1 className="name">{`${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)}`}</h1>
+              <PokemonData url={pokemon.url} />
+              <button
+                onClick={() => setMyPokemon(prev => prev.filter(item => item !== pokemon))}
+              >
+                Fjern
+              </button>
+            </div>
+          )
+        })
+      )
+    }
+  }
+
+  const handleOnChange = (e) => {
+    setQuery(e.target.value)
+  }
 
   return (
     <>
       <main>
         <Banner />
-        <Search />
+        <div className="search--wrapper">
+            <form className="search--form" action="">
+                <input 
+                    className="search--input" 
+                    placeholder="Finn pokemon..." 
+                    value={query}
+                    type="text" 
+                    onChange={handleOnChange}
+                />
+                <button className="search--btn">Finn pokemon!</button>
+            </form>
+        </div>
+        <p>myPokemon.length:{myPokemon.length}</p>
+        <div className='list'>
+          {myPokemon && listElements(myPokemon)}
+        </div>
         <div className='cards'>
-          {allPokemon ? cardElements() : console.log('theres no pokemon in the list')}
+          {query ? cardElements(searchResults) : cardElements(allPokemon)}
         </div>
       </main>
     </>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,14 @@
-export default function Search() {
+export default function Search(props) {
     return (
         <div className="search--wrapper">
             <form className="search--form" action="">
-                <input className="search--input" placeholder="Finn pokemon..." type="name" />
+                <input 
+                    className="search--input" 
+                    placeholder="Finn pokemon..." 
+                    value={props.query} 
+                    type="name" 
+                    onChange={props.handleOnChange}
+                />
                 <button className="search--btn">Finn pokemon!</button>
             </form>
         </div>
