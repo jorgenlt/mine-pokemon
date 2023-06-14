e2d52ccdc97a4de2c1db3d4eb0913199953bbd09
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-06-14 17:08:54 +0200

    redux-persist + clean-up

--- src/features/pokemons/PokemonCardContent.jsx
+++ src/features/pokemons/PokemonCardContent.jsx
@@ -1,16 +1,26 @@
 export default function PokemonCardContent(props) {
+
+  const abilitiesList = (abilities) => {
+    return (
+      abilities.map(element => <li key={element.ability.name}>{element.ability.name}</li>)
+    )
+  }
   
   return (
     <>
-      <div className='card--image'>
-        <img src={props.image} alt="Pokemon"></img>
-      </div>
-      <div className='card--abilities'>
-        <h3>Ferdigheter</h3>
-        <ul>
-          {props.abilities && props.abilities.map(element => <li key={element.ability.name}>{element.ability.name}</li>)}
-        </ul>
-      </div>
+      {props.abilities && (
+        <>
+          <div className='card--image'>
+            <img src={props.image} alt="Pokemon"></img>
+          </div>
+          <div className='card--abilities'>
+            <h3>Ferdigheter</h3>
+            <ul>
+              {abilitiesList(props.abilities)}
+            </ul>
+          </div>
+        </>
+      )}
     </>
   )
 }
--- src/features/pokemons/PokemonsList.jsx
+++ src/features/pokemons/PokemonsList.jsx
@@ -1,40 +1,41 @@
 import { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
-import { selectAllPokemons, fetchPokemons } from './pokemonsSlice'
+import { selectAllPokemons, fetchAllPokemons } from './pokemonsSlice'
 import PokemonCard from './PokemonCard'
+import Loader from '../../components/Loader'
 
 export default function PokemonList() {
   const dispatch = useDispatch();
 
-  const pokemons = useSelector(selectAllPokemons);
+  const allPokemons = useSelector(selectAllPokemons);
   const { 
     status, 
     error, 
-    filteredPokemons, 
+    filteredAllPokemons, 
     searchQuery 
   } = useSelector(state => state.pokemons);
 
   useEffect(() => {
     if (status === 'idle') {
-      dispatch(fetchPokemons());
+      dispatch(fetchAllPokemons());
     }
   },[status, dispatch])
 
   let content;
 
   if (status === 'loading') {
-    content = <img src="/pikachu.png" className='suspense-loading' alt="pikachu"></img>
+    content = <Loader />
   } else if (status === 'succeeded') {
-      content = pokemons.map(pokemon => <PokemonCard key={pokemon.name} pokemon={pokemon} />)
+      content = allPokemons.map(pokemon => <PokemonCard key={pokemon.name} pokemon={pokemon} />)
   } else if (status === 'failed') {
     content = <div>{error}</div>
   }
   
   return (
     <section className="pokemon-list">
-      {filteredPokemons.length > 0 ? (
-        filteredPokemons.map(pokemon => <PokemonCard key={pokemon.name} pokemon={pokemon} />)
+      {filteredAllPokemons.length > 0 ? (
+        filteredAllPokemons.map(pokemon => <PokemonCard key={pokemon.name} pokemon={pokemon} />)
       ) : (
         searchQuery !== '' ? (
           <p>Finner ingen Pokemon med det navnet. Prøv å endre søket ditt.</p>
